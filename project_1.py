43b321575e11de59416e64ec0e72931963d98b5e
Mugilan001 <amugilan001@gmail.com>
2025-05-04 21:04:18 +0530

    Update project_1.py

--- project_1.py
+++ project_1.py
@@ -59,29 +59,3 @@
 ##    main()
 
 
-import threading
-import time
-
-def walk_dog(first,last):
-    time.sleep(8)
-    print(f"you finish walking {first} {last}")
-
-def take_out_trash():
-    time.sleep(2)
-    print("You take out the trash")
-
-def get_mail():
-    time.sleep(4)
-    print("You get the mail")
-
-chore1=threading.Thread(target=walk_dog,args=("scooby","doggy",))
-chore1.start()
-
-chore2=threading.Thread(target=take_out_trash)
-chore2.start()
-
-chore3=threading.Thread(target=get_mail)
-chore3.start()
-
-
-
