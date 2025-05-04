4fe22509f806cc074f74a48624222126092353d5
Mugilan001 <amugilan001@gmail.com>
2025-05-04 07:55:13 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,29 @@
+##operation=int(input("enter operation value:"))
+##if operation==1:
+##     a=int(input("Enter your first number:"))
+##     b=int(input("Enter your second number:"))
+##     print("The sum of two number is :",a+b)
+##elif operation==2:
+##     a=int(input("Enter your first number:"))
+##     b=int(input("Enter your second number:"))
+##     print("The subtraction of two number is :",a-b)
+##elif operation==3:
+##     a=int(input("Enter your first number:"))
+##     b=int(input("Enter your second number:"))
+##     print("The multiplication of two number is :",a*b)
+##elif operation==4:
+##     a=int(input("Enter your first number:"))
+##     b=int(input("Enter your second number:"))
+##     print("The divition of two number is :",a/b)
+##else:
+##    print("invalid entry")
+     
+    
+
+thelist=[10,20,30,40,50]
+thelist.pop(1)
+print(thelist)
+del thelist[0]
+print(thelist)
+thelist.reverse()
+print(thelist)
